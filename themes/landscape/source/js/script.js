3bc7e78f70c1dbf5584129ae652e66c74d4c30d7
Timon Wong <timon86.wang@gmail.com>
2014-05-15 11:40:55 +0800

    Add share button for Sina Weibo

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -59,6 +59,7 @@
             '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
             '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
             '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
           '</div>',
         '</div>'
       ].join('');
